014c5498cd0b7693385d20d3bffb493bb3f87a3b
jaggusuperhit <144913058+jaggusuperhit@users.noreply.github.com>
2025-03-30 20:33:26 +0530

    Add files via upload
    let/const (block-scoped variables)
    
    Template literals (`Hello ${name}`)
    
    Destructuring (const { prop } = obj)
    
    Modules (import/export)
    
    Promises/async-await

---
+++ app.js
@@ -0,0 +1,10 @@
+alert("Hello World");
+
+var email = prompt("Enter your email");
+var password = prompt("Enter your password");
+
+if (email == "abc@campusx.in" && password == "1234"){
+    alert("Welcome");
+} else {
+    alert("Not Correct");
+}
